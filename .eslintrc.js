fdd5a4d8dd19bfe21d2b8a4a2af6c49b5dbe4a7a
Steven Freeman <sfreeman422@protonmail.com>
2019-11-05 16:09:16 -0500

    Adjusted eslintrc to remove unnecessary typescript stuff (#3)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   extends: ['airbnb-base'],
   settings: {
-    'import/extensions': [".js", ".jsx", ".ts", ".tsx"],
-    'import/parsers': {
-      '@typescript-eslint/parser': [".ts", ".tsx"]
-    },
+    'import/extensions': [".js"],
     'import/resolver': {
       'node': {
-        'extensions': [".js", ".jsx", ".ts", ".tsx"]
+        'extensions': [".js"]
       }
     }
   }
