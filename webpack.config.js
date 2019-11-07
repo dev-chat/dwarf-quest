fd30da0bdad5b919e22f8a80aae8502922ed12f8
Maxwell Lasky <mlasky46@gmail.com>
2019-11-07 15:22:38 -0700

    Improves webpack config (#8)
    * uses webpack for module bundling
    
    * Adds hashing
    
    * Removes dist folder
    
    * More improvements

--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,56 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/main.js',
-    vendors: ['phaser'],
+    vendors: ['phaser']
   },
 
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js']
   },
 
   output: {
-    filename: 'app.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename:
+      process.env.NODE_ENV === 'production'
+        ? 'app.bundle.[hash].js'
+        : 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
 
   mode: 'development',
 
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
-    port: 3000,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|tmx|tsx)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'file-loader',
+        type: 'javascript/auto'
+      }
+    ]
   },
 
   plugins: [
     new CopyWebpackPlugin([
       {
         from: path.resolve(__dirname, 'index.html'),
-        to: path.resolve(__dirname, 'dist'),
-      },
-      {
-        from: path.resolve(__dirname, 'assets', '**', '*'),
-        to: path.resolve(__dirname, 'dist'),
-      },
+        to: path.resolve(__dirname, 'dist')
+      }
     ]),
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true),
-    }),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    })
   ],
 
   optimization: {
@@ -47,9 +63,9 @@ module.exports = {
         commons: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
+          chunks: 'all'
+        }
+      }
+    }
+  }
 };
