7575eabd6bcec56f437bb6696e193c4b341e4c7e
Maxwell Lasky <mlasky46@gmail.com>
2019-11-08 21:26:00 -0700

    Small update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 npm run build
 
-git add dist && git commit -m "dist subtree commit" --no-verify
+git add dist -f && git commit -m "dist subtree commit" --no-verify
 
-git subtree push --prefix dist origin gh-pages
+# adopted from https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
+git push origin `git subtree split --prefix dist master`:gh-pages --force
