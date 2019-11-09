1b7c29cbad787329fff3f25bb1f57c9c973a8fc1
Maxwell Lasky <mlasky46@gmail.com>
2019-11-08 22:14:43 -0700

    Removes /dist from source

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ npm run build
 
 git add dist -f && git commit -m "dist subtree commit" --no-verify
 
-# adopted from https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
-git push origin `git subtree split --prefix dist`:gh-pages --force
+git subtree push --prefix dist origin gh-pages
