5cc7ab67214b77056d8a5c5af5e36ca4f214254f
Maxwell Lasky <mlasky46@gmail.com>
2019-11-08 21:42:02 -0700

    Updates deploy script and removes debug lines

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npm run build
 git add dist -f && git commit -m "dist subtree commit" --no-verify
 
 # adopted from https://gist.github.com/tduarte/eac064b4778711b116bb827f8c9bef7b
-git push origin `git subtree split --prefix dist master`:gh-pages --force
+git push origin `git subtree split --prefix dist`:gh-pages --force
